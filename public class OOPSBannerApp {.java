bfc7612af25a2deb4989253b7c0c78221029a1df
ss3008 <ss4057@srmist.edu.in>
2026-03-02 12:50:52 +0530

    UC6

---
+++ public class OOPSBannerApp {.java
@@ -0,0 +1,62 @@
+public class OOPSBannerApp {
+
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+            "  ***** ",
+            " * * ",
+            "* *",
+            "* *",
+            "* *",
+            " * * ",
+            "  ***** "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+            "******* ",
+            "* * ",
+            "* * ",
+            "******* ",
+            "* ",
+            "* ",
+            "* "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+            " ******* ",
+            "* ",
+            "* ",
+            " ******* ",
+            "        *",
+            "        *",
+            " ******* "
+        };
+    }
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+
+        // Declare String Arrays to hold patterns for each letter
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Use the loop to Assemble each line of the banner to create the
+        // visual effect for the message "OOPS"
+        for (int i = 0; i < oPattern.length; i++) {
+            // Concatenate patterns: O + O + P + S
+            System.out.println(
+                oPattern[i] + "  " + 
+                oPattern[i] + "  " + 
+                pPattern[i] + "  " + 
+                sPattern[i]
+            );
+        }
+    }
+}
